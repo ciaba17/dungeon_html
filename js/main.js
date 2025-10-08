e31805208622b735fa13cdea74e410ed2db51275
ciaba17 <gabrycdp@gmail.com>
2025-10-08 20:29:48 +0200

    Aggiunti file e struttura principali di html, css e js

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// Variabili globali che servono nel gioco
+const gameState = {
+    currentScene: null,
+    playerHP: 100,
+  // altre proprietà che vuoi tracciare
+};
+
+// Funzione che inizializza il gioco
+function initGame() {
+    console.log("Inizio il gioco");
+    gameState.currentScene = "intro";  // nome scena iniziale
+    changeScene(gameState.currentScene);
+}
+
+// Funzione per cambiare scena
+function changeScene(sceneName) {
+    console.log("Cambio scena a:", sceneName);
+    // TODO: nascondi elementi, carica dati, mostra nuovi elementi
+    // es: scenes.loadScene(sceneName);
+    // ui.updateForScene(sceneName);
+}
+
+// Quando la pagina è caricata, avvia il gioco
+window.addEventListener("DOMContentLoaded", () => {
+    initGame();
+});
